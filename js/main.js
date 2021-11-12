4f697dfdc15fdebd692469e2cc2ede620adc6900
Davinci9T <igonevv@gmail.com>
2021-11-12 20:10:40 +0500

    .

--- js/main.js
+++ js/main.js
@@ -227,7 +227,7 @@ var evSlide = new Swiper(".events-swiper", {
       spaceBetween: 50,
     },
 
-    769: {
+    989: {
       slidesPerView: 3,
       spaceBetween: 27,
     },
@@ -239,6 +239,88 @@ var evSlide = new Swiper(".events-swiper", {
   }
 });
 
+// EDITIONS =====================================================================================================================
+
+// SELECT-CHECKBOX
+
+let button = ".h2-click";
+let labels = ".checkbox--label";
+let labelsList = ".checkbox-list";
+let labelsListActive = "checklist-active";
+let labelActive = "checkbox--label-active";
+let animationClass = "animation-test";
+let inputCheckbox = ".checkbox";
+
+function checkboxToggle(a, b, c, labelsListActive, labelActive, animationClass, inputCheckbox) {
+  let btn = document.querySelector(a);
+  let labels = document.querySelectorAll(b);
+  let listLabels = document.querySelector(c);
+  btn.addEventListener("click", toggleSpoiler);
+  btn.addEventListener("keyup", function (e) {
+    console.log(e.key);
+    if (e.code === "Enter") {
+      toggleSpoiler();
+    }
+  })
+  function toggleSpoiler() {
+    if (!listLabels.classList.contains(labelsListActive)) {
+      listLabels.classList.add(labelsListActive);
+      labels.forEach(item => {
+        // item.classList.add("checkbox--label-active");
+        animationItem(item, labelActive, animationClass, "add");
+      })
+    } else {
+      listLabels.classList.remove(labelsListActive);
+      labels.forEach(item => {
+        if (item.querySelector(inputCheckbox).checked) {
+          animationItem(item, labelActive, animationClass, "add");
+        } else {
+          animationItem(item, labelActive, animationClass, "remove");
+        }
+      });
+    }
+    labels.forEach(item => {
+      item.addEventListener("click", function () {
+        if (!listLabels.classList.contains(labelsListActive)) {
+          animationItem(this, labelActive, animationClass, "remove");
+        }
+      });
+    })
+  }
+  function animationItem(item, class1, class2, f) {
+    if (f === "add") {
+      item.classList.add(class1);
+      setTimeout(function () {
+        item.classList.add(class2)
+      }, 100);
+
+    } else {
+      item.classList.remove(class2);
+      setTimeout(function () {
+        item.classList.remove(class1)
+      }, 300);
+    }
+
+  }
+
+
+}
+
+
+
+checkboxToggle(button, labels, labelsList, labelsListActive, labelActive, animationClass, inputCheckbox);
+
+// открытие селекта
+
+$(function () {
+  $('.h2-click').on('click', function () {
+    $('.h2-click').toggleClass('active');
+    $('.checkbox-list').toggleClass('active');
+  });
+
+});
+
+// SWIPER
 
 let editionsSlider = new Swiper(".editions__swiper-content", {
   pagination: {
@@ -262,10 +344,24 @@ let editionsSlider = new Swiper(".editions__swiper-content", {
       spaceBetween: 49,
     },
 
-    560: {
+    718: {
       slidesPerView: 2,
       slidesPerGroup: 2,
       spaceBetween: 34,
+      grid: {
+        rows: 1,
+        fill: "row"
+      },
+    },
+
+    560: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 30,
+      grid: {
+        rows: 4,
+        fill: "row"
+      },
     },
   }
 });
