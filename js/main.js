770d9da8df42eb049ccae08132819bd9a3be4a17
Davinci9T <igonevv@gmail.com>
2021-12-03 18:59:37 +0500

    /

--- js/main.js
+++ js/main.js
@@ -392,83 +392,102 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // SELECT-CHECKBOX
 
-let button = ".h2-click";
-let labels = ".editions__label-checkbox";
-let labelsList = ".editions__list-checkbox";
-let labelsListActive = "checklist-active";
-let labelActive = "editions__label-checkbox-active";
-let animationClass = "animation-test";
-let inputCheckbox = ".checkbox";
+// let button = ".h2-click";
+// let labels = ".editions__label-checkbox";
+// let labelsList = ".editions__list-checkbox";
+// let labelsListActive = "checklist-active";
+// let labelActive = "editions__label-checkbox-active";
+// let animationClass = "animation-test";
+// let inputCheckbox = ".checkbox";
 
-function checkboxToggle(a, b, c, labelsListActive, labelActive, animationClass, inputCheckbox) {
-  let btn = document.querySelector(a);
-  let labels = document.querySelectorAll(b);
-  let listLabels = document.querySelector(c);
-  btn.addEventListener("click", toggleSpoiler);
-  btn.addEventListener("keyup", function (e) {
-    console.log(e.key);
-    if (e.code === "Enter") {
-      toggleSpoiler();
-    }
-  });
+// function checkboxToggle(a, b, c, labelsListActive, labelActive, animationClass, inputCheckbox) {
+//   let btn = document.querySelector(a);
+//   let labels = document.querySelectorAll(b);
+//   let listLabels = document.querySelector(c);
+//   btn.addEventListener("click", toggleSpoiler);
+//   btn.addEventListener("keyup", function (e) {
+//     console.log(e.key);
+//     if (e.code === "Enter") {
+//       toggleSpoiler();
+//     }
+//   });
 
-  function toggleSpoiler() {
-    if (!listLabels.classList.contains(labelsListActive)) {
-      listLabels.classList.add(labelsListActive);
-      labels.forEach(item => {
-        // item.classList.add("editions__label-checkbox-active");
-        animationItem(item, labelActive, animationClass, "add");
-      });
-    } else {
-      listLabels.classList.remove(labelsListActive);
-      labels.forEach(item => {
-        if (item.querySelector(inputCheckbox).checked) {
-          animationItem(item, labelActive, animationClass, "add");
-        } else {
-          animationItem(item, labelActive, animationClass, "remove");
-        }
-      });
-    }
-    labels.forEach(item => {
-      item.addEventListener("click", function () {
-        if (!listLabels.classList.contains(labelsListActive)) {
-          animationItem(this, labelActive, animationClass, "remove");
-        }
-      });
+//   function toggleSpoiler() {
+//     if (!listLabels.classList.contains(labelsListActive)) {
+//       listLabels.classList.add(labelsListActive);
+//       labels.forEach(item => {
+//         // item.classList.add("editions__label-checkbox-active");
+//         animationItem(item, labelActive, animationClass, "add");
+//       });
+//     } else {
+//       listLabels.classList.remove(labelsListActive);
+//       labels.forEach(item => {
+//         if (item.querySelector(inputCheckbox).checked) {
+//           animationItem(item, labelActive, animationClass, "add");
+//         } else {
+//           animationItem(item, labelActive, animationClass, "remove");
+//         }
+//       });
+//     }
+//     labels.forEach(item => {
+//       item.addEventListener("click", function () {
+//         if (!listLabels.classList.contains(labelsListActive)) {
+//           animationItem(this, labelActive, animationClass, "remove");
+//         }
+//       });
+//     });
+//   }
+
+//   function animationItem(item, class1, class2, f) {
+//     if (f === "add") {
+//       item.classList.add(class1);
+//       setTimeout(function () {
+//         item.classList.add(class2);
+//       }, 100);
+
+//     } else {
+//       item.classList.remove(class2);
+//       setTimeout(function () {
+//         item.classList.remove(class1);
+//       }, 300);
+//     }
+
+//   }
+
+
+// }
+
+
+// checkboxToggle(button, labels, labelsList, labelsListActive, labelActive, animationClass, inputCheckbox);
+
+// // открытие селекта
+
+// $(function () {
+//   $('.h2-click').on('click', function () {
+//     $('.h2-click').toggleClass('active');
+//     $('.editions__list-checkbox').toggleClass('active');
+//   });
+
+// });
+
+// (() => {
+//   const checkBtn = document.querySelector('.js-check-heading');
+
+//   checkBtn.addEventListener('click', function () {
+//     this.classList.toggle('is-active');
+//   });
+// })();
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  $(function () {
+    $('.js-check-heading').on('click', function () {
+      $('.js-check-heading').toggleClass('is-active');
     });
-  }
-
-  function animationItem(item, class1, class2, f) {
-    if (f === "add") {
-      item.classList.add(class1);
-      setTimeout(function () {
-        item.classList.add(class2);
-      }, 100);
-
-    } else {
-      item.classList.remove(class2);
-      setTimeout(function () {
-        item.classList.remove(class1);
-      }, 300);
-    }
-
-  }
-
-
-}
-
-
-checkboxToggle(button, labels, labelsList, labelsListActive, labelActive, animationClass, inputCheckbox);
-
-// открытие селекта
-
-$(function () {
-  $('.h2-click').on('click', function () {
-    $('.h2-click').toggleClass('active');
-    $('.editions__list-checkbox').toggleClass('active');
   });
-
 });
+
+
 
 // SWIPER издания
 
