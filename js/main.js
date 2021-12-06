951f7c19c210c8576782526b6661055ac9393b1c
Davinci9T <igonevv@gmail.com>
2021-12-06 20:09:40 +0500

    .

--- js/main.js
+++ js/main.js
@@ -390,94 +390,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // EDITIONS =====================================================================================================================
 
-// SELECT-CHECKBOX
-
-// let button = ".h2-click";
-// let labels = ".editions__label-checkbox";
-// let labelsList = ".editions__list-checkbox";
-// let labelsListActive = "checklist-active";
-// let labelActive = "editions__label-checkbox-active";
-// let animationClass = "animation-test";
-// let inputCheckbox = ".checkbox";
-
-// function checkboxToggle(a, b, c, labelsListActive, labelActive, animationClass, inputCheckbox) {
-//   let btn = document.querySelector(a);
-//   let labels = document.querySelectorAll(b);
-//   let listLabels = document.querySelector(c);
-//   btn.addEventListener("click", toggleSpoiler);
-//   btn.addEventListener("keyup", function (e) {
-//     console.log(e.key);
-//     if (e.code === "Enter") {
-//       toggleSpoiler();
-//     }
-//   });
-
-//   function toggleSpoiler() {
-//     if (!listLabels.classList.contains(labelsListActive)) {
-//       listLabels.classList.add(labelsListActive);
-//       labels.forEach(item => {
-//         // item.classList.add("editions__label-checkbox-active");
-//         animationItem(item, labelActive, animationClass, "add");
-//       });
-//     } else {
-//       listLabels.classList.remove(labelsListActive);
-//       labels.forEach(item => {
-//         if (item.querySelector(inputCheckbox).checked) {
-//           animationItem(item, labelActive, animationClass, "add");
-//         } else {
-//           animationItem(item, labelActive, animationClass, "remove");
-//         }
-//       });
-//     }
-//     labels.forEach(item => {
-//       item.addEventListener("click", function () {
-//         if (!listLabels.classList.contains(labelsListActive)) {
-//           animationItem(this, labelActive, animationClass, "remove");
-//         }
-//       });
-//     });
-//   }
-
-//   function animationItem(item, class1, class2, f) {
-//     if (f === "add") {
-//       item.classList.add(class1);
-//       setTimeout(function () {
-//         item.classList.add(class2);
-//       }, 100);
-
-//     } else {
-//       item.classList.remove(class2);
-//       setTimeout(function () {
-//         item.classList.remove(class1);
-//       }, 300);
-//     }
-
-//   }
-
-
-// }
-
-
-// checkboxToggle(button, labels, labelsList, labelsListActive, labelActive, animationClass, inputCheckbox);
-
-// // открытие селекта
-
-// $(function () {
-//   $('.h2-click').on('click', function () {
-//     $('.h2-click').toggleClass('active');
-//     $('.editions__list-checkbox').toggleClass('active');
-//   });
-
-// });
-
-// (() => {
-//   const checkBtn = document.querySelector('.js-check-heading');
-
-//   checkBtn.addEventListener('click', function () {
-//     this.classList.toggle('is-active');
-//   });
-// })();
-
 
 document.addEventListener('DOMContentLoaded', function () {
   $(function () {
@@ -485,55 +397,175 @@ document.addEventListener('DOMContentLoaded', function () {
       $('.js-check-heading').toggleClass('is-active');
     });
   });
+
+  // let editionsSlider = new Swiper(".editions__swiper-content", {
+  //   pagination: {
+  //     el: ".editions-pagination",
+  //     type: "fraction"
+  //   },
+  //   navigation: {
+  //     nextEl: ".button-next",
+  //     prevEl: ".button-prev"
+  //   },
+  //   breakpoints: {
+  //     1200: {
+  //       slidesPerView: 3,
+  //       slidesPerGroup: 3,
+  //       spaceBetween: 50,
+  //     },
+
+  //     769: {
+  //       slidesPerView: 2,
+  //       slidesPerGroup: 2,
+  //       spaceBetween: 49,
+  //     },
+
+  //     718: {
+  //       slidesPerView: 2,
+  //       slidesPerGroup: 2,
+  //       spaceBetween: 34,
+  //       grid: {
+  //         rows: 1,
+  //         fill: "row"
+  //       },
+  //     },
+
+  //     600: {
+  //       slidesPerView: 2,
+  //       slidesPerGroup: 2,
+  //       spaceBetween: 0,
+  //       grid: {
+  //         rows: 4,
+  //         fill: "row"
+  //       },
+  //     },
+  //   }
+  // });
+
+  (() => {
+    const MOBILE_WIDTH = 600;
+
+    const sliderParamsNotMobile = {
+      sliderWrap: "editions__swiper",
+      cardsContainerName: "editions__swiper-content",
+      cardsWrapName: "editions-wrapper",
+      card: "editions-item",
+      paginationClassName: "editions-pagination",
+      navClassName: "editions-navigation",
+      navBtnClassName: "nav-btn",
+      navPrev: "button-prev",
+      navNext: "button-next"
+    };
+
+    function getWindowWidth() {
+      console.log(document.body.scrollWidth);
+      return Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.body.clientWidth,
+        document.documentElement.clientWidth
+      );
+    }
+
+    function activateSlider(params) {
+      const navigation = document.createElement("div");
+      const pagination = document.createElement("div");
+      const navBtnPrev = document.createElement("button");
+      const navBtnNext = document.createElement("button");
+
+      navigation.classList.add(params.navClassName);
+
+      navBtnPrev.classList.add(params.navBtnClassName);
+      navBtnPrev.classList.add(params.navPrev);
+      navigation.prepend(navBtnPrev);
+
+      pagination.classList.add(params.paginationClassName);
+      navigation.append(pagination);
+
+      navBtnNext.classList.add(params.navBtnClassName);
+      navBtnNext.classList.add(params.navNext);
+      navigation.append(navBtnNext);
+
+      params.sliderWrapElem.prepend(navigation);
+
+      params.cardsContainer.classList.add("swiper-container");
+      params.cardsWrap.classList.add("swiper-wrapper");
+
+      params.cardsSlider = new Swiper(`.${params.cardsContainerName}`, {
+        slidesPerView: 2,
+        spaceBetween: 20,
+
+        pagination: {
+          el: `.${params.sliderWrap} .${params.paginationClassName}`,
+          type: "fraction"
+        },
+
+        navigation: {
+          nextEl: `.${params.navNext}`,
+          prevEl: `.${params.navPrev}`
+        },
+
+        on: {
+          beforeInit() {
+            document.querySelectorAll(`.${params.card}`).forEach((el) => {
+              el.classList.add("swiper-slide");
+            });
+          },
+
+          beforeDestroy() {
+            this.slides.forEach((el) => {
+              el.classList.remove("swiper-slide");
+              el.removeAttribute("role");
+              el.removeAttribute("aria-label");
+            });
+
+            this.pagination.el.remove();
+            navigation.remove();
+          }
+        }
+      });
+    }
+
+    function destroySlider(params) {
+      params.cardsSlider.destroy();
+      params.cardsContainer.classList.remove("swiper-container");
+      params.cardsWrap.classList.remove("swiper-wrapper");
+      params.cardsWrap.removeAttribute("aria-live");
+      params.cardsWrap.removeAttribute("id");
+    }
+
+    function checkWindowWidth(params) {
+      const currentWidth = getWindowWidth();
+      params.sliderWrapElem = document.querySelector(`.${params.sliderWrap}`);
+      params.cardsContainer = document.querySelector(
+        `.${params.cardsContainerName}`
+      );
+      params.cardsWrap = document.querySelector(`.${params.cardsWrapName}`);
+
+      if (
+        currentWidth > MOBILE_WIDTH &&
+        (!params.cardsSlider || params.cardsSlider.destroyed)
+      ) {
+        activateSlider(params);
+      } else if (currentWidth <= MOBILE_WIDTH && params.cardsSlider) {
+        destroySlider(params);
+      }
+    }
+
+    checkWindowWidth(sliderParamsNotMobile);
+
+    window.addEventListener("resize", function () {
+      checkWindowWidth(sliderParamsNotMobile);
+    });
+  })();
+
+
 });
 
 
-
-// SWIPER издания
-
-let editionsSlider = new Swiper(".editions__swiper-content", {
-  pagination: {
-    el: ".editions-pagination",
-    type: "fraction"
-  },
-  navigation: {
-    nextEl: ".button-next",
-    prevEl: ".button-prev"
-  },
-  breakpoints: {
-    1200: {
-      slidesPerView: 3,
-      slidesPerGroup: 3,
-      spaceBetween: 50,
-    },
-
-    769: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 49,
-    },
-
-    718: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 34,
-      grid: {
-        rows: 1,
-        fill: "row"
-      },
-    },
-
-    560: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 0,
-      grid: {
-        rows: 4,
-        fill: "row"
-      },
-    },
-  }
-});
+// TOOLtips
 
 
 tippy('#tooltip-one', {
